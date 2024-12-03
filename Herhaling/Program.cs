1003572420c37e11b480df39af3b279e019ad684
Arthur <lotuslovesurmom@gmail.com>
2024-12-03 10:51:12 +0100

    herhaling 04/00

---
+++ Herhaling/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Herhaling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            double[] prijzen = new double[] { 0.99, 5.60, 10.10,15.00 };
+
+            string[] artiekelen = { "snoepje", "luxe broodje", "lunch menu","big lunch menu"};
+
+            Formulier[] formulieren = new Formulier[2];
+
+            formulieren[0] = new Formulier()
+            {
+                Feedback = "prijzijg maar lekker",
+                Sterren = 5
+            };
+
+            formulieren[1] = new Formulier()
+            {
+                Feedback = "gwn lekker",
+                Sterren = 5
+            };
+
+            for (int i = 0; i < prijzen.Length; i++) 
+            {
+                Console.WriteLine(prijzen[i]);
+                Console.WriteLine(artiekelen[i]);
+            }
+
+            foreach (Formulier formulier in formulieren)
+            {
+                Console.WriteLine(formulier.Feedback);
+            }
+
+
+        }
+
+ 
+        
+     
+    }
+}
---
+++ Herhaling/formulier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herhaling
+{
+    internal class Formulier
+    {
+        internal int Sterren {  get; set; }
+        internal string Feedback { get; set; }
+    }
+
+}
