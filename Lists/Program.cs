6c040935751b93cdaba5d7f8b02932e515c20983
Arthur <lotuslovesurmom@gmail.com>
2024-12-04 00:29:22 +0100

    mob made 04/04

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -83,6 +83,8 @@ namespace Lists
             {
                 Console.WriteLine(review);
             }
+
+
         }
     }
 }
---
+++ Lists/mob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    internal class mob
+    {
+        internal bool isdead;
+        internal int hp = 10;
+    }
+}
