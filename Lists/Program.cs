c44b9348792e53dec263bd0021aefe57ac11f637
Arthur <lotuslovesurmom@gmail.com>
2024-12-03 12:35:24 +0100

    04/01

---
+++ Lists/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+        }
+
+        internal void Run()
+        {
+            List<string> characterList = new List<string>();
+            characterList.Add("Sleepy joe");
+
+            string[] characters = {
+                "Pac-Man",
+                "Samus",
+                "Crash Bandicoot",
+                "Spyro the Dragon",
+                "Donkey Kong",
+                "Mario",
+                "Luigi",
+                "Astro",
+                "Solid Snake",
+                "Master Chief",
+                "Kratos",
+                "Sonic the Hedgehog",
+                "Link"
+            };
+
+            foreach (string character in characters)
+            {
+                characterList.Add(character);
+            }
+
+            Console.WriteLine("Character List:");
+            foreach (string character in characterList)
+            {
+                Console.WriteLine(character);
+            }
+        }
+    }
+}
